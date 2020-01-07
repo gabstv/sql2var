8ede0511cb1177b283db3100bcd58218a1d44b9f
Gabriel Ochsenhofer <gabs@gabs.tv>
2020-01-07 09:46:36 -0300

    simple quotes

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
@@ -14,5 +16,33 @@ b `
 	v2 := getslicecode(raw, ";")
 	if v != v2 {
 		t.Fatal(v, v2)
+	}
+}
+
+func TestSimpleQuotes(t *testing.T) {
+	rawf := `
+-- define: test1
+aa bb "--cc" "-d--dd"
+ee "--" ff
+-- end
+	`
+	mk := make([]string, 0)
+	mv := make([]string, 0)
+	mtags := make([][]string, 0)
+	f, err := ioutil.TempFile("", "simplequotes")
+	if err != nil {
+		t.Fatal(err.Error())
+		return
+	}
+	fnm := f.Name()
+	f.Write([]byte(rawf))
+	f.Close()
+	defer os.Remove(fnm)
+	extractall([]string{fnm}, &mk, &mv, &mtags)
+	if len(mk) != 1 || len(mv) != 1 {
+		t.Fatal("invalid len")
+	}
+	if mv[0] != "aa bb \"--cc\" \"-d--dd\"\nee \"--\" ff\n" {
+		t.Fatal("invalid chars", mv[0])
 	}
 }
